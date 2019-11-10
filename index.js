43f4842f02904edd1f4ae79ee7695fd5d2898447
arkhipovds <arkhipovds@yandex.ru>
2019-11-10 15:51:06 +0300

    Changed type of datatime from String to Number (UT in ms)

--- index.js
+++ index.js
@@ -2,39 +2,15 @@
 const { ApolloServer, gql } = require("apollo-server");
 const mongoose = require("mongoose");
 
-/* 
-//TODO вернуться к работе с форматом Date
+//server's parameters
+const parameters = new Object({
+  DBHost: "10.76.70.51",
+  DBPort: "27017",
+  DBName: "test"
+});
 
-const { GraphQLScalarType } = require("graphql");
-const { Kind } = require("graphql/language");
-
-//Determine type Date for GraphQL
-const resolverMap = {
-  Date: new GraphQLScalarType({
-    name: 'Date',
-    description: 'Date custom scalar type',
-    parseValue(value) {
-      return new Date(value); // value from the client
-    },
-    serialize(value) {
-      return value.getTime(); // value sent to the client
-    },
-    parseLiteral(ast) {
-      if (ast.kind === Kind.INT) {
-        return new Date(ast.value) // ast value is always in string format
-      }
-      return null;
-    },
-  }),
-};
-*/
-
-//DB server's parameters                 !!!!!!!!!!!!!!!!!!!!!!!!!!TODO заюзать это в коде
-const DBServer = new Object({
-  host: '10.76.70.51',
-  port: '27017',
-  DB: 'test'
-})
+parameters.host = process.argv[2] ? process.argv[2] : "localhost";
+parameters.port = process.argv[3] ? process.argv[3] : "4000";
 
 //Determine structure for employees
 const employeeSchema = new mongoose.Schema({
@@ -48,69 +24,80 @@ const employeeSchema = new mongoose.Schema({
     type: Boolean,
     default: true
   }
-  
 });
 
-//Determine structure for shifts              !!!!!!!!!!!! employeeId нужно перевести на тип "type: Schema.Types.ObjectId, ref: 'Employee'""
+//Determine structure for shifts              !!!!!!!!!!!! TODO: employeeId нужно перевести на тип "type: Schema.Types.ObjectId, ref: 'Employee'""
 const shiftSchema = new mongoose.Schema({
-  start: String,
-  end: String,
-  employeeId: String,
-  test: Date
+  start: Number,
+  end: Number,
+  employeeId: String
 });
 
-//Compile models with schema "employeeSchema" and collection name "Employees"
+//Compile model with schema "employeeSchema" and collection name "Employees"
 const modelEmployee = mongoose.model("Employees", employeeSchema);
-//Compile models with schema "shiftSchema" and collection name "Shifts"
+//Compile model with schema "shiftSchema" and collection name "Shifts"
 const modelShift = mongoose.model("Shifts", shiftSchema);
 
-//connect to mongodb-server 10.76.70.51 on port 27017 to DB "test"         !!!!!!!!!!!!вынести все в параметры
+//connect to mongodb-server
 mongoose
   .connect(
-    "mongodb://10.76.70.51:27017/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useFindAndModify: false }
+    "mongodb://" +
+      parameters.DBHost +
+      ":" +
+      parameters.DBPort +
+      "/" +
+      parameters.DBName +
+      "?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    }
   )
-  .then(() => console.log("База взлетела!"))                    // !!!!!!!!!!!!сделать лог информативнее
+  .then(() => console.log('Connected to DB "' + parameters.DBName + '"'))
   .catch(err => console.error(err));
 
-//Declare types for graphql-queries
+//Declare types and queries for graphql-queries
 const typeDefs = gql`
-  scalar Date
   type Employee {
     id: String
-    """ Last name """
     fullName: String
-    """ Is regular employee? """
     isRegular: Boolean
-    """ Цвет отображения """
     visibleColor: String
-    """ Is not fired """
     isActive: Boolean
   }
   type Shift {
     id: String
-    """ When shift starts """
     start: String
-    """ When shift ends """
     end: String
-    """ Id of employee who is responsible for the shift"""
     employeeId: String
-    test: Date
   }
   type Query {
     Employees: [Employee]
     activeEmployees: [Employee]
-    Shifts: [Shift]
-    shiftsForMonth (month:String): [Shift]
+    Shifts(utPointInMonth: String): [Shift]
   }
   type Mutation {
-    addShift(start:String, end:String, employeeId:String): Shift!
-    updateShift(id:String, start:String, end:String, employeeId:String): Shift!
-    deleteShift(id:String): Boolean!
+    addShift(start: String, end: String, employeeId: String): Shift!
+    updateShift(
+      id: String
+      start: String
+      end: String
+      employeeId: String
+    ): Shift!
+    deleteShift(id: String): Boolean!
 
-    addEmployee(fullName:String, isRegular:Boolean, visibleColor:String): Employee!
-    updateEmployee(id:String, fullName:String, isRegular:Boolean, visibleColor:String): Employee!
-    deleteEmployee(id:String): Boolean!
+    addEmployee(
+      fullName: String
+      isRegular: Boolean
+      visibleColor: String
+    ): Employee!
+    updateEmployee(
+      id: String
+      fullName: String
+      isRegular: Boolean
+      visibleColor: String
+    ): Employee!
+    deleteEmployee(id: String): Boolean!
   }
 `;
 
@@ -119,25 +106,25 @@ const resolvers = {
   Query: {
     Employees: async (_, args, { Employee }) => {
       const employees = await modelEmployee.find();
-      console.log(employees);
-	    return employees;
+      return employees;
     },
     activeEmployees: async (_, args, { Employee }) => {
-      const employees = await modelEmployee.find({ isActive:true});
-      console.log(employees);
-	    return employees;
+      const employees = await modelEmployee.find({ isActive: true });
+      return employees;
     },
-    //TODO отдавать смены за период, а не все
-    Shifts: async (_, args, { Shift }) => {
-	    const days = await modelShift.find({});
-    	return days;
-    },
-    shiftsForMonth: async (_, {month}, { Shift }) => {
-	    const days = await modelShift.find({});
-    	return days;
-    },
+    Shifts: async (_, { utPointInMonth }, { Shift }) => {
+      //Задаем отклонение от указанного времени 40 суток
+      const delta = 3456000000;
+      console.log(msToDateString(utPointInMonth));
+      console.log(utPointInMonth);
+      const days = await modelShift.find({
+        start: { $gte: utPointInMonth - delta, $lte: utPointInMonth + delta }
+      });
+      return days;
+    }
   },
   Mutation: {
+    //TODO запретить смены длиннее 48 часов и отрицательные смены
     addShift: async (_, { start, end, employeeId }, { Shift }) => {
       const newShift = await new modelShift({
         start,
@@ -146,24 +133,29 @@ const resolvers = {
       }).save();
       return newShift;
     },
+    //TODO запретить смены длиннее 48 часов и отрицательные смены
     updateShift: async (_, { id, start, end, employeeId }, { Shift }) => {
       let shift = await modelShift.findOneAndUpdate(
-        {_id:id}, 
+        { _id: id },
         {
-          start:start, 
-          end:end, 
-          employeeId:employeeId
+          start: start,
+          end: end,
+          employeeId: employeeId
         },
-        {new: true}
+        { new: true }
       );
       return shift;
     },
     deleteShift: async (_, { id }, { Shift }) => {
-      shift = await modelShift.findOneAndRemove({ _id:id });
+      shift = await modelShift.findOneAndRemove({ _id: id });
       return shift ? true : false;
     },
 
-    addEmployee: async (_, { fullName, isRegular, visibleColor }, { Employee }) => {
+    addEmployee: async (
+      _,
+      { fullName, isRegular, visibleColor },
+      { Employee }
+    ) => {
       const newEmployee = await new modelEmployee({
         fullName,
         isRegular,
@@ -171,28 +163,32 @@ const resolvers = {
       }).save();
       return newEmployee;
     },
-    updateEmployee: async (_, { id, fullName, isRegular, visibleColor }, { Employee }) => {
+    updateEmployee: async (
+      _,
+      { id, fullName, isRegular, visibleColor },
+      { Employee }
+    ) => {
       let employee = await modelEmployee.findOneAndUpdate(
-        {_id:id}, 
+        { _id: id },
         {
-          fullName:fullName, 
-          isRegular:isRegular, 
-          visibleColor:visibleColor
+          fullName: fullName,
+          isRegular: isRegular,
+          visibleColor: visibleColor
         },
-        {new: true}
+        { new: true }
       );
       return employee;
     },
     deleteEmployee: async (_, { id }, { Employee }) => {
       let employee = await modelEmployee.findOneAndUpdate(
-        {_id:id}, 
+        { _id: id },
         {
-          isActive:false
+          isActive: false
         },
-        {new: true}
+        { new: true }
       );
       return employee ? true : false;
-    },
+    }
   }
 };
 
@@ -204,8 +200,18 @@ const server = new ApolloServer({
 });
 
 //start it
-const HOST = process.argv[2];  //!!!!!!!!!дефолтное значение задать
-const PORT = process.argv[3];  //!!!!!!!!!дефолтное значение задать
-server.listen({ host: HOST, port: PORT }).then(({ url }) => {
-  console.log(`Взлетел ${url}`);
-});
+server
+  .listen({ host: parameters.host, port: parameters.port })
+  .then(({ url }) => {
+    console.log(`Ready for GraphQL-queries on ${url}`);
+  });
+
+//Преобразовывает UNIX-time (в мс) в строку в формате "YYYY-MM-DD HH:MM"
+function msToDateString(ms) {
+  const tempDate = new Date(Number.parseInt(ms));
+  const tempString =
+    tempDate.toISOString().slice(0, 10) +
+    " " +
+    tempDate.toISOString().slice(11, 16);
+  return tempString;
+}
